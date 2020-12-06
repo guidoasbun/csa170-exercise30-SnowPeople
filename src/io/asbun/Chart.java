c3c89a0a0756b147176267be91b2b8873b171d26
Guido Asbun <gdrodrigo8@gmail.com>
2020-12-06 14:51:12 -0800

    scarves

--- src/io/asbun/Chart.java
+++ src/io/asbun/Chart.java
@@ -20,18 +20,27 @@ public class Chart extends JComponent{
         g.fillOval(175,300,120, 120);
         g.fillOval(155,400,150,150);
         g.fillOval(130,518, 200,200);
+            //Hat
         g.setColor(Color.decode("#0a043c"));
         g.fillRect(192,240, 90,65);
         g.fillRect(170,305,135,12);
+            //Scarf
+        g.setColor(Color.decode("#aa3a3a"));
+        g2.setStroke(new BasicStroke(25));
+        g2.draw(new Line2D.Float(200,410,265,410));
+        g2.draw(new Line2D.Float(260,415,275,430));
             //Arms
         g.setColor(Color.BLACK);
         g2.setStroke(new BasicStroke(3));
             //Right
         g2.draw(new Line2D.Float(160,450,130,465));
-        g2.draw(new Line2D.Float(130,465,120,550));
+        g2.draw(new Line2D.Float(130,465,120,540));
+        g.setColor(Color.decode("#aa3a3a"));
+        g.fillOval(110,525,20,30);
             //Left
+        g.setColor(Color.BLACK);
         g2.draw(new Line2D.Float(300,450,330,465));
-        g2.draw(new Line2D.Float(330,465,340,550));
+        g2.draw(new Line2D.Float(330,465,340,540));
 
 
         //Snow-mom
@@ -39,21 +48,48 @@ public class Chart extends JComponent{
         g.fillOval(395,340,100, 100);
         g.fillOval(375,425,130,130);
         g.fillOval(350,530, 180,180);
+            //Scarf
+        g.setColor(Color.decode("#aa3a3a"));
+        g2.setStroke(new BasicStroke(25));
+        g2.draw(new Line2D.Float(415,433,470,433));
+        g2.draw(new Line2D.Float(470,440,480,450));
             //Arms
         g.setColor(Color.BLACK);
         g2.setStroke(new BasicStroke(3));
             //Right
         g2.draw(new Line2D.Float(386,455,355,465));
-        g2.draw(new Line2D.Float(355,465,340,550));
+        g2.draw(new Line2D.Float(355,465,340,540));
+        g.setColor(Color.decode("#aa3a3a"));
+        g.fillOval(330,525,20,30);
             //Left
+        g.setColor(Color.BLACK);
         g2.draw(new Line2D.Float(495,455,528,465));
-        g2.draw(new Line2D.Float(528,465,540,550));
+        g2.draw(new Line2D.Float(528,465,540,540));
+
 
         //Snow-daughter
         g.setColor(Color.decode("#f6f6f6"));
         g.fillOval(578,515,45, 45);
         g.fillOval(565,550,70,70);
         g.fillOval(550,600, 100,100);
+            //Scarf
+        g.setColor(Color.decode("#ffc7c7"));
+        g2.setStroke(new BasicStroke(10));
+        g2.draw(new Line2D.Float(585,553,615,553));
+        g2.draw(new Line2D.Float(615,556,625,560));
+            //Arms
+        g.setColor(Color.BLACK);
+        g2.setStroke(new BasicStroke(3));
+            //Right
+        g2.draw(new Line2D.Float(570,570,540,540));
+        g.setColor(Color.decode("#aa3a3a"));
+        g.fillOval(530,525,20,30);
+            //Left
+        g.setColor(Color.BLACK);
+        g2.draw(new Line2D.Float(630,570,655,600));
+        g.setColor(Color.decode("#aa3a3a"));
+        g.fillOval(646,590,10,20);
+
 
 
     }
